1ff57fa32939fa02c18e0baa7801202be7a82b86
Pandeyashish17 <asespandey2@gmail.com>
2023-02-24 10:01:29 +0545

    meta tags added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="PickUp Lines Generator using GPT-3" />
+        <meta
+          name="description"
+          content="Unleash your charm with our Pickup Lines Generator powered by GPT-3! Find clever, flirty, and romantic lines in just a few clicks. Try it now!"
+        />
+        <meta
+          name="keywords"
+          content="pickup lines, generator, GPT-3, first impression, icebreaker, flirting, romance, AI-powered, conversation starter, charm"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Ashish Pandey" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
